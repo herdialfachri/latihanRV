bf0b03695d170e770d94ce8aa9d125a0f1f08b73
herdialfachri <alfachri75@gmail.com>
2024-11-12 09:11:18 +0700

    first commit

---
+++ app/src/main/java/com/herdialfachri/myapplication/ProdukActivity.kt
@@ -0,0 +1,27 @@
+package com.herdialfachri.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class ProdukActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_produk)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rvProduk)
+
+        // Set GridLayoutManager dengan jumlah kolom 2
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+    }
+}
