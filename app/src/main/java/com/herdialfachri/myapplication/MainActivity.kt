a3d37b9962450d041964c9a28fd3aba21c37a231
herdialfachri <alfachri75@gmail.com>
2024-11-25 16:52:43 +0700

    Adding toolbar from product to main

--- app/src/main/java/com/herdialfachri/myapplication/MainActivity.kt
+++ app/src/main/java/com/herdialfachri/myapplication/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.herdialfachri.myapplication
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +20,18 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.rvProduk)
+
+        // Set GridLayoutManager dengan jumlah kolom 2
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+
+        // Tambahkan OnClickListener untuk TextView dengan ID tvFavicon
+        val tvFavicon: TextView = findViewById(R.id.tvFavicon)
+        tvFavicon.setOnClickListener {
+            val intent = Intent(this@MainActivity, ProductActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/herdialfachri/myapplication/ProductActivity.kt
+++ app/src/main/java/com/herdialfachri/myapplication/ProductActivity.kt
@@ -1,27 +1,31 @@
 package com.herdialfachri.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
-class ProdukActivity : AppCompatActivity() {
+class ProductActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_produk)
+        setContentView(R.layout.activity_product)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val recyclerView = findViewById<RecyclerView>(R.id.rvProduk)
+        val toolbar: Toolbar = findViewById(R.id.toolbar_detail)
+        setSupportActionBar(toolbar)
 
-        // Set GridLayoutManager dengan jumlah kolom 2
-        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        toolbar.setNavigationOnClickListener {
+            val intent = Intent(this@ProductActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
